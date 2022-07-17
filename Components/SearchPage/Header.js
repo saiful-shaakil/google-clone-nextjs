992cc142170fb4d6d63a666dc6ae0551c4478b9b
si-shakil <sishakil850@gmail.com>
2022-07-17 15:26:12 +0600

    dependency updated

--- Components/SearchPage/Header.js
+++ Components/SearchPage/Header.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { useRef } from "react";
 import Avatar from "../Avatar";
-import profile from "../../images/profile.jpg";
 import HeaderOptions from "./HeaderOptions";
 function Heade() {
   const router = useRouter();
